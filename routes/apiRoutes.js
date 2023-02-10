1e047e4f510451130e601e2a912e972b3d033572
Hurtorc <anire88@gmail.com>
2023-02-09 20:30:53 -0800

    changed up the routes again to fix post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,37 @@
 const router = require("express").Router();
 const fs = require("fs");
+const uuid = require("uuid");
 
 // GET request for notes
-router.get("/notes", (req, res) => {
-  console.log(`GET request for notes: ${req.body}`);
-  res.sendFile(path.join(__dirname, "../db/db.json"));
-  JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
-});
-
-// POST request for notes
-router.post("/notes", (req, res) => {
-  console.log(`POST request for notes: ${req.body}`);
-  let newNote = req.body;
+router.get("/", (req, res) => {
+  console.log(`GET request for notes: ${req.method}`);
   let notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
-  notes.push(newNote);
-  fs.writeFileSync("../db/db.json", JSON.stringify(notes));
   res.json(notes);
 });
 
-router.delete("/notes/:id", (req, res) => {
-  console.log(`DELETE request for notes: ${req.body}`);
+// POST request for notes
+router.post("/", (req, res) => {
+  console.log(`POST request for notes: ${req.method}`);
+  const newNote = ({ title, text, id } = req.body);
+  newNote.id = uuid.v4();
+
+  const previousNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  previousNotes.push(newNote);
+
+  fs.writeFileSync("../db/db.json", JSON.stringify(previousNotes), (err) => {
+    err ? console.error(err) : console.log("Note added successfully!");
+  });
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(`DELETE request for notes: ${req.method}`);
   let notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
   notes.splice(req.params.id, 1);
   fs.writeFileSync("../db/db.json", JSON.stringify(notes));
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/api", apiRoutes);
+app.use("/api/notes", apiRoutes);
 
 //GET request for index page
 app.get("/", (req, res) => {
