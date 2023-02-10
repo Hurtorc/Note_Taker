a214cf98d4c3796a956b153a135859a2992e3ec4
Hurtorc <anire88@gmail.com>
2023-02-09 20:06:50 -0800

    routes and server working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,19 +3,27 @@ const fs = require("fs");
 
 // GET request for notes
 router.get("/notes", (req, res) => {
-    console.log(`GET request for notes: ${req.body}`);
-    res.sendFile(path.join(__dirname, "../db/db.json"));
-    JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  console.log(`GET request for notes: ${req.body}`);
+  res.sendFile(path.join(__dirname, "../db/db.json"));
+  JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
 });
-
 
 // POST request for notes
 router.post("/notes", (req, res) => {
-    console.log(`POST request for notes: ${req.body}`);
-    let newNote = req.body;
-    let notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
-    notes.push(newNote);
-    fs.writeFileSync("../db/db.json", JSON.stringify(notes));
-    res.json(notes);
+  console.log(`POST request for notes: ${req.body}`);
+  let newNote = req.body;
+  let notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  notes.push(newNote);
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes));
+  res.json(notes);
 });
 
+router.delete("/notes/:id", (req, res) => {
+  console.log(`DELETE request for notes: ${req.body}`);
+  let notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  notes.splice(req.params.id, 1);
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes));
+  res.json(notes);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require("path");
 
 const apiRoutes = require("./routes/apiRoutes");
 
@@ -14,12 +15,12 @@ app.use("/api", apiRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-//GET request for notes page
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //Spinning up the server
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`server now on port ${PORT}!`);
 });
