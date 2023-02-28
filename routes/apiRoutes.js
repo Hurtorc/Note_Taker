fcc6ef0af68a8057271c7a1ba30384079ecb0173
Hurtorc <anire88@gmail.com>
2023-02-28 14:50:19 -0800

    added uuidv4 instead of making a helper function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const fs = require("fs");
-const uuid = require("../utils/uuid.js");
+const uuid = require("uuid");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "..", "db", "db.json");
 
 // GET request for notes
 router.get("/", (req, res) => {
   console.log(`GET request for notes: ${req.method}`);
-  const notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  const notes = JSON.parse(fs.readFileSync(dbPath, "utf8"));
   res.json(notes);
 });
 
@@ -15,10 +18,10 @@ router.post("/", (req, res) => {
   const newNote = ({ title, text, id } = req.body);
   newNote.id = uuid.v4();
 
-  const previousNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  const previousNotes = JSON.parse(fs.readFileSync(dbPath, "utf8"));
   previousNotes.push(newNote);
 
-  fs.writeFileSync("../db/db.json", JSON.stringify(previousNotes), (err) => {
+  fs.writeFileSync(dbPath, JSON.stringify(previousNotes), (err) => {
     err ? console.error(err) : console.log("Note added successfully!");
   });
   const response = {
@@ -32,15 +35,14 @@ router.post("/", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   console.log(`DELETE request for notes: ${req.method}`);
-  const notes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+  const notes = JSON.parse(fs.readFileSync(dbPath, "utf8"));
   const noteIndex = notes.findIndex((note) => note.id === req.params.id);
 
   if (noteIndex !== -1) {
     notes.splice(noteIndex, 1);
-  fs.writeFileSync("../db/db.json", JSON.stringify(notes));
-  res.json(notes);   
-  }
-  else {
+    fs.writeFileSync(dbPath, JSON.stringify(notes));
+    res.json(notes);
+  } else {
     res.status(404).json({ message: "Note not found" });
   }
 });
--- utils/uuid.js
+++
@@ -1,4 +0,0 @@
-//create a function that generates a unique id
-const uuid = () => {
-  return Math.random().toString(36).slice(2);
-};
